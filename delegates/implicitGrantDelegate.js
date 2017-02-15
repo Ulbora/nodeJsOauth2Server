08153f2c31153b8e4cee108866bc9fd553c529a3
ken <kenulborapublic@gmail.com>
2017-02-14 22:01:07 -0500

    fixed issue with refresh token delegate

--- delegates/implicitGrantDelegate.js
+++ delegates/implicitGrantDelegate.js
@@ -34,77 +34,46 @@ exports.init = function (database) {
 
 exports.createImplicitGrant = function (json, scopes, callback) {
     var rtn = {
-        id: null,        
+        id: null,
         success: false,
         message: null
     };
     var clientId = json.clientId;
     var userId = json.userId;
-    // create refresh token
-    //var refreshPayload = {
-       // sub: "code",
-       // userId: userId,
-       // clientId: clientId
-   // };
-   // console.log("refresh token refreshPayload: " + JSON.stringify(refreshPayload));
-    //refreshTokenDelegate.generateRefreshToken(refreshPayload, function (refreshToken) {
-        //console.log("refresh token: " + refreshToken);
-        //if (refreshToken) {
-            //var rftJson = {
-               // token: refreshToken
-            //};
-            db.getClientRoleAllowedUriListByClientId(clientId, function (clientRoleUriList) {
-                console.log("clientRoleUriList: " + JSON.stringify(clientRoleUriList));
-                if (clientRoleUriList) {
-                    var roleUriList = [];
-                    for (var cnt = 0; cnt < clientRoleUriList.length; cnt++) {
-                        roleUriList.push(clientRoleUriList[cnt]);
-                    }
-                    var accessPayload = {
-                        sub: "access",
-                        userId: userId,
-                        clientId: clientId,
-                        roleUris: roleUriList,
-                        scopeList: scopes,
-                        expiresIn: config.IMPLICIT_ACCESS_TOKEN_LIFE
+    db.getClientRoleAllowedUriListByClientId(clientId, function (clientRoleUriList) {
+        console.log("clientRoleUriList: " + JSON.stringify(clientRoleUriList));
+        if (clientRoleUriList) {
+            var roleUriList = [];
+            for (var cnt = 0; cnt < clientRoleUriList.length; cnt++) {
+                roleUriList.push(clientRoleUriList[cnt]);
+            }
+            var accessPayload = {
+                sub: "access",
+                userId: userId,
+                clientId: clientId,
+                roleUris: roleUriList,
+                scopeList: scopes,
+                expiresIn: config.IMPLICIT_ACCESS_TOKEN_LIFE
+            };
+            accessTokenDelegate.generateAccessToken(accessPayload, function (accessToken) {
+                if (accessToken) {
+                    var dateNow = new Date();
+                    var acTokenExpires = new Date(dateNow.getTime() + (config.IMPLICIT_ACCESS_TOKEN_LIFE * 60000));
+                    var accessTknJson = {
+                        token: accessToken,
+                        expires: acTokenExpires
                     };
-                    accessTokenDelegate.generateAccessToken(accessPayload, function (accessToken) {
-                        if (accessToken) {
-                            var dateNow = new Date();
-                           // var acExpires = new Date(dateNow.getTime() + 300000);
-                            var acTokenExpires = new Date(dateNow.getTime() + (config.IMPLICIT_ACCESS_TOKEN_LIFE * 60000));
+                    // var randonCode = generateRandonAuthCode();
+                    var implicitJson = {
+                        clientId: clientId,
+                        userId: userId
 
-                            var accessTknJson = {
-                                token: accessToken,
-                                expires: acTokenExpires
-                            };
-                           // var randonCode = generateRandonAuthCode();
-                            var implicitJson = {
-                                clientId: clientId,
-                                userId: userId
-
-                            };
-                            db.addImplicitGrant(implicitJson, accessTknJson, scopes, function (implicitResult) {
-                                if (implicitResult.success) {
-                                    //var codeString = generateAuthCodeString(addAccessTknResult.authorizationCode, addAccessTknResult.codeString);
-                                    //var updateJson = {
-                                       // randonAuthCode: codeString,
-                                       // alreadyUsed: false,
-                                       // authorizationCode: addAccessTknResult.authorizationCode
-                                   // }
-                                    //db.updateAuthorizationCode(updateJson, function (authCodeUpdateResult) {
-                                      //  if (authCodeUpdateResult.success) {
-                                            rtn.id = implicitResult.id;                                            
-                                            rtn.success = true;
-                                            callback(rtn);
-                                       // } else {
-                                         //   callback(rtn);
-                                      //  }
-                                   // });
-                                } else {
-                                    callback(rtn);
-                                }
-                            });
+                    };
+                    db.addImplicitGrant(implicitJson, accessTknJson, scopes, function (implicitResult) {
+                        if (implicitResult.success) {
+                            rtn.id = implicitResult.id;
+                            rtn.success = true;
+                            callback(rtn);
                         } else {
                             callback(rtn);
                         }
@@ -113,33 +82,8 @@ exports.createImplicitGrant = function (json, scopes, callback) {
                     callback(rtn);
                 }
             });
-        //} else {
-            //callback(rtn);
-       // }
-   // });
+        } else {
+            callback(rtn);
+        }
+    });
 };
-
-/*
-
-var generateRandonAuthCode = function () {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < 20; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-};
-
-
-var generateAuthCodeString = function (authCode, randonString) {
-    var rtn = null;
-    if (authCode && randonString) {
-        var fpart = randonString.substring(0, 7);
-        var spart = randonString.substring(8);
-        rtn = fpart + authCode + spart;
-    }
-    return rtn;
-};
-   
-   */
