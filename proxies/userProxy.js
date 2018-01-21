2d99fa6364a4769942573a9c368aefc59fce8aa7
ken <kenulborapublic@gmail.com>
2018-01-21 12:12:08 -0500

    updates

--- proxies/userProxy.js
+++ proxies/userProxy.js
@@ -2,19 +2,22 @@ var constants = require("../constants/constants");
 var request = require('request');
 
 exports.login = function (loginJson, callback) {
+    console.log(loginJson);
     var rtn = {
         valid: false,
         message: ""
     };
     var url = process.env.AUTHENTICATION_SERVICE || constants.AUTHENTICATION_SERVICE_LOCAL;
+    console.log(url);
     var options = {
         method: 'post',
         body: loginJson,
         json: true,
-        forever: true,
-        timeout: 120000,
+        //forever: true,
+        //timeout: 120000,
         url: url
     };
+    console.log(options);
     request(options, function (err, res, body) {
         if (!err && body) {
             var statusCode = res.statusCode;
